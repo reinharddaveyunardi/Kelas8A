03893c8590eda9125cb7c064142de84e7ee28dba
Web File 8 <126077909+WebKelas8A@users.noreply.github.com>
2023-04-19 21:34:59 +0700

    Add files via upload

---
+++ main/js/script.js
@@ -0,0 +1,12 @@
+const navbarNav = document.querySelector('.navbar-nav');
+document.querySelector('#menu').
+onclick = () => {
+    navbarNav.classList.toggle('active')
+};
+
+const menu = document.querySelector('#menu');
+document.addEventListener('click', function(e){
+    if(!menu.contains(e.target) && !navbarNav.contains(e.target)){
+        navbarNav.classList.remove('active');
+    }
+});
