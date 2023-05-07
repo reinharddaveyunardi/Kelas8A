6ff8204cd2f923c076fe4704aa354f6bb7d3b3e5
Web File 8 <126077909+WebKelas8A@users.noreply.github.com>
2023-05-07 12:01:35 +0700

    Update script.js

--- main/js/script.js
+++ main/js/script.js
@@ -1,12 +1,42 @@
+//Menu
 const navbarNav = document.querySelector('.navbar-nav');
 document.querySelector('#menu').
-onclick = () => {
-    navbarNav.classList.toggle('active')
+onclick = (e) => {
+    navbarNav.classList.toggle('active');
+    e.preventDefault();
+    
 };
 
+
+//Search
+
+
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e) => {
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
+}
+
+
+//klik luar element
+
 const menu = document.querySelector('#menu');
+const sb = document.querySelector('#search-button');
+
 document.addEventListener('click', function(e){
     if(!menu.contains(e.target) && !navbarNav.contains(e.target)){
         navbarNav.classList.remove('active');
     }
 });
+
+//sb
+
+document.addEventListener('click', function(e){
+    if(!sb.contains(e.target) && !searchForm.contains(e.target)){
+        searchForm.classList.remove('active');
+    }
+});
+
