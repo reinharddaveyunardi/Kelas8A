f7f847c4946fa0d49e2ae51e0cba8e85bf890791
Web File 8 <126077909+WebKelas8A@users.noreply.github.com>
2023-05-14 17:54:26 +0700

    Add files via upload

--- main/js/app.js
+++ main/js/app.js
@@ -1,17 +1,17 @@
 function search() {
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById('search-box');
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("all");
-    li = ul.getElementsByTagName('section');
-  
-    for (i = 0; i < li.length; i++) {
-      a = li[i].getElementsByTagName("h3")[0];
-      txtValue = a.textContent || a.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        li[i].style.display = "";
-      } else {
-        li[i].style.display = "none";
-      }
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('search-box');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("all");
+  li = ul.getElementsByClassName("student-card");
+
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("h3")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
     }
   }
+}
